3da186dacaa2964cc51b3acbdce7b8b962dfc06f
EC2 Default User <ec2-user@ip-172-31-90-195.ec2.internal>
2019-08-29 03:41:26 +0000

    next commit

---
+++ labwork/COSW30-Lab1.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+    <head>
+            <title>My Name is Gaith</title>
+            
+    </head>git 
+    <body>
+        <h1>Gaith Cattan</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim lobortis scelerisque fermentum dui. Non odio euismod lacinia
+            at quis risus sed vulputate odio. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Non nisi est sit amet facilisis. Cursus metus aliquam eleifend mi in nulla posuere sollicitudin.
+            Elementum nibh tellus molestie nunc non blandit massa. Cras sed felis eget velit aliquet. Porta non pulvinar neque laoreet suspendisse interdum consectetur. Curabitur gravida arcu ac tortor.
+            Porttitor lacus luctus accumsan tortor. Odio eu feugiat pretium nibh ipsum consequat. Pretium lectus quam id leo in vitae turpis massa. Ipsum dolor sit amet consectetur adipiscing elit
+            pellentesque habitant. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Non tellus orci ac auctor augue. Diam vulputate ut pharetra sit amet aliquam id. In nisl nisi scelerisque eu.</p>
+            
+        <?php
+        echo 'a href="">This is My PHP paragraph</a>'
+        ?>
+        
+    </body>
+    
+    
+</html>
