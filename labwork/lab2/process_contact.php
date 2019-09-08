de562befae50cc4b43ca2d821c72ac9145c00917
EC2 Default User <ec2-user@ip-172-31-90-195.ec2.internal>
2019-09-08 19:21:10 +0000

    third

--- labwork/lab2/process_contact.php
+++ labwork/lab2/process_contact.php
@@ -1,18 +1,21 @@
 <?php
 
 
-// validate if the fields are empty
-    if(!empty($_POST['full_name']) && !empty($_POST['email']) && !empty($_POST['gender']) && !empty($_POST['age_range']))
-    {
-
    $full_name = $_POST['full_name'];
    $email = $_POST['email'];
    $gender = $_POST['gender'];
    $age_range = $_POST['age_range'];
 
 
-    echo '<h2>Congratulation you have registered correctly.</h2>';
+// validate if the fields are empty
+    if(!empty($_POST['full_name']) && !empty($_POST['email']) && !empty($_POST['gender']) && !empty($_POST['age_range']))
+    {
 
+        echo '<h2>Congratulation you have registered correctly.</h2>';
+
+    }
+    else {
+        echo '<h2>Incomplete form. Please try again.</h2>';
     }
 
 
