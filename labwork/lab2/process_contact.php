0d689529f7bc0e3c97a3c1a9a16cde6a29d6fd48
EC2 Default User <ec2-user@ip-172-31-90-195.ec2.internal>
2019-09-08 19:54:51 +0000

    third

--- labwork/lab2/process_contact.php
+++ labwork/lab2/process_contact.php
@@ -1,17 +1,22 @@
 <?php
 
 
-   $full_name = $_POST['full_name'];
-   $email = $_POST['email'];
-   $gender = $_POST['gender'];
-   $age_range = $_POST['age_range'];
-
 
 // validate if the fields are empty
     if(!empty($_POST['full_name']) && !empty($_POST['email']) && !empty($_POST['gender']) && !empty($_POST['age_range']))
     {
 
         echo '<h2>Congratulation you have registered correctly.</h2>';
+
+       $full_name = $_POST['full_name'];
+       $email = $_POST['email'];
+       $gender = $_POST['gender'];
+       $age_range = $_POST['age_range'];
+       $age_rangecontribution_amount = $_POST['contribution_amount'];
+       $comment = $_POST["comment"];
+       $mailing_list = $_POST['mailing_list'];
+
+
 
     }
     else {
