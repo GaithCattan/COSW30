74a6cb95d69788b9a0f44ad7a38c248df51d8c05
EC2 Default User <ec2-user@ip-172-31-90-195.ec2.internal>
2019-09-17 02:57:00 +0000

    fourth-2

--- labwork/lab2/process_tuitioncalculator.php
+++ labwork/lab2/process_tuitioncalculator.php
@@ -12,16 +12,20 @@ $health_fee = 20;
 $services_card = $_POST['services_card'];
 $parking_permit = $_POST['parking_permit'];
 
-$total_2 = $total_1 + $services_card + $parking_permit;
+$total_2 = $total_1 + $health_fee + $services_card + $parking_permit;
 
 
 echo "Cost of Tuition: $units_num * $residency = $total_1";
+echo "<br>";
+echo "<h2>Extra Fees: </h2>"
 echo "<br>";
 echo "Student Health Fee: $$health_fee";
 echo "<br>";
 echo "College Services card: $$services_card";
 echo "<br>";
 echo "Parking Permit: $$parking_permit";
+echo "<br><br>";
+echo "<h2>Final Cost: </h2>"
 echo "<br>";
 echo "Total College Balance Due: $total_2";
 
