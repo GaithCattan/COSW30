2ec71ff008739b4c5c9ecae74bf78af9535647ab
EC2 Default User <ec2-user@ip-172-31-90-195.ec2.internal>
2019-09-17 03:11:21 +0000

    fourth-2

--- labwork/lab2/process_tuitioncalculator.php
+++ labwork/lab2/process_tuitioncalculator.php
@@ -14,7 +14,7 @@ $parking_permit = $_POST['parking_permit'];
 
 $total_2 = $total_1 + $health_fee + $services_card + $parking_permit;
 
-$scholarship_award = rand(0,$total_2);
+$scholarship_award = rand(0,$total_1);
 
 $total_3 = $total_2 - $scholarship_award;
 
@@ -22,7 +22,6 @@ $total_3 = $total_2 - $scholarship_award;
 echo "Cost of Tuition: $units_num units * $$residency = <b>$$total_1</b>";
 echo "<br>";
 echo "<h2>Extra Fees: </h2>";
-echo "<br>";
 echo "Student Health Fee: $$health_fee";
 echo "<br>";
 echo "College Services card: $$services_card";
@@ -30,7 +29,6 @@ echo "<br>";
 echo "Parking Permit: $$parking_permit";
 echo "<br><br>";
 echo "<h2>Final Cost: </h2>";
-echo "<br>";
 echo "Total Registration Costs: <b>$$total_2</b>";
 echo "<br>";
 echo "Scholarship Award: $$scholarship_award";
