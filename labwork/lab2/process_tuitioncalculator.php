29d16336d17fab72fcd6f43f551ebc6f3e3ff4d8
EC2 Default User <ec2-user@ip-172-31-90-195.ec2.internal>
2019-09-19 02:49:45 +0000

    fourth-2

--- labwork/lab2/process_tuitioncalculator.php
+++ labwork/lab2/process_tuitioncalculator.php
@@ -1,3 +1,29 @@
+<!doctype html>
+<html>
+    <head>
+         <meta charset="utf-8">
+         <title>Tuition Calculator</title>
+        <style>
+          body {background-color: black; background-image: url("https://upload.wikimedia.org/wikipedia/commons/f/fa/LBCC_LAC_Bldg_A.jpg");
+                background-position: center;
+                background-repeat: no-repeat;}
+          h1 {text-align: center; font-size: 60px; margin-top: 40px;
+              border-radius: 25px; margin-left: auto; margin-right: auto;
+              background-color: white;}
+          #container {width: 900px;	margin-left: auto;	margin-right: auto;	padding-top: 50px;	padding-bottom: 150px; border-radius: 25px;	background-color: #DA6B40;}
+          #texting {width: 500px; display: block; margin-left: auto; margin-right: auto;}
+          input[type=submit] {background-color: #33733F; border: none; border-radius: 10px;  color: white;  padding: 16px 32px;  text-decoration: none;  margin: 4px 2px;  cursor: pointer;}
+          footer {font-size: 1.5em; color: white; text-align: center; margin-top: 11%;}
+        </style>
+            
+    </head>
+    <body>
+
+    <div id="container">
+    <div id="texting">
+
+    <h1>Tuition Calculator</h1>
+
 <?php
 
 
@@ -39,5 +65,13 @@ echo "Total College Balance Due: <b>$$total_3</b>";
 ?>
 
 
+    </div>
+    </div>
+    </body>
+    
+    <footer>
+        <p>&copy; 2019 by Gaith Cattan</p>
+    </footer>
 
+</html>
 
---
+++ labwork/lab4/generator.php
@@ -0,0 +1,61 @@
+<?php
+
+
+    $numParagraph = $_POST['paragraphs'];
+    $text = $_POST['text'];
+    
+    $textExplode = explode(' ', $text);
+
+    shuffle($textExplode);
+    
+    
+//  $array = [ 1 => 'test', 'test'];
+    
+    
+    foreach($textExplode as $key => $value) {
+        echo "<p>$key Value: $value</p>";
+        //<p>1 Value: word</p>
+    }
+
+    // use the implode function to turn it back into a string
+    
+    $textImplode = implode(' ',$textExplode);
+    echo "<p>$textImplode</p>";
+
+
+
+
+
+$text = "Bacon ipsum dolor amet shank meatball kielbasa, shoulder rump doner ground round tail pig picanha. Turkey tongue chicken, buffalo boudin tail shank shankle. Corned beef turducken strip steak, meatball chuck
+sirloin spare ribs pastrami cupim. Porchetta bacon jerky ham hock tail bresaola pork chop picanha.
+Jowl beef sausage pancetta meatloaf fatback. Landjaeger filet mignon jerky, pork chop pastrami kielbasa meatloaf leberkas pig boudin beef ribs. Biltong pork belly short loin tail brisket, pork loin filet mignon
+buffalo bacon t-bone beef ribs rump turkey drumstick. T-bone cupim pork belly cow bacon boudin ball tip pancetta beef corned beef meatball. Cupim drumstick picanha boudin, jowl corned beef chicken tri-tip.";
+
+
+
+  for($i = 0; $i < $numParagraph; $i++) {
+        echo "<p>$text</p>";
+        echo '<p>'. $text .'</p>';}
+
+
+
+//    echo 'Text Explode';
+//    print_r($textExplode);
+
+
+//    $shuffleText = shuffle($textExplode);
+    
+//    echo 'Text Shuffled';    
+//    print_r($shuffleText);
+    
+
+
+
+
+
+//Other Loops
+    // foreach()
+    // while()
+
+?>
+
