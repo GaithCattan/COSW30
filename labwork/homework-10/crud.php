8198d4e52acfd8fd8f5544c88789601f298c612b
GaithCattan <gaith.c@gmail.com>
2019-11-07 02:01:46 +0000

    change in the table

--- labwork/homework-10/crud.php
+++ labwork/homework-10/crud.php
@@ -118,7 +118,7 @@ if($result) {
           #container {width: 900px;	margin-left: auto;	margin-right: auto;	padding-top: 50px;	padding-bottom: 150px; border-radius: 25px;	background-color: #00EBCC;}
           #texting {width: 500px; display: block; margin-left: auto; margin-right: auto; text-align: center;}
           input[type=submit] {background-color: grey; border: none; border-radius: 10px;  color: white;  padding: 16px 32px;  text-decoration: none;  margin: 4px 2px;  cursor: pointer;}
-          table, th, td {border: 1px solid black; text-align: center;}
+          table, th, td {border: 1px solid black; text-align: center; margin-left: 20%; margin-right: 20%;}
           footer {font-size: 1.5em; color: white; text-align: center; margin-top: 11%;}
         </style>
 </head>
