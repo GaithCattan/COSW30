75985181cd16f54d6b8dc293a858a7d06f0a52be
GaithCattan <gaith.c@gmail.com>
2019-11-13 02:58:41 +0000

    final changes

--- labwork/homework-10/delete.php
+++ labwork/homework-10/delete.php
@@ -1,25 +1,15 @@
 <?php
-// Add the database connection
+
 include('database.php');
-/*
-*   CHECK IF THE URL HAS A $_GET VARIABLE CALLED ID
-*/
-// homework/update.php?id=3
-if(isset($_GET['id'])) {
-    $id = $_GET['id'];
-} else {
-    // redirect to crud.php
-    header('Location: crud.php');
-    exit;
-}
+
+$id = $_GET['id'];
+
+$delete_query = "DELETE FROM USER_CATTAN WHERE user_id= '$id' ";
 
 
-$delete_query = "DELETE FROM Customers WHERE user_id='$id'";
+$delete_result = mysqli_query($connection, $delete_query);
 
-
-$delete_result = mysqli_query($connection, $update_query);
-
-if($update_result) {
+if($delete_result) {
     header('Location: crud.php');
 } else {
     echo 'No delete happen';
